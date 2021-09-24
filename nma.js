9439d4c1060744823afd4d62e9b2881a418fe959
rahul patil <60380578+patil-rahuls@users.noreply.github.com>
2021-09-24 13:20:54 +0530

    fixed styles

--- nma.js
+++ nma.js
@@ -348,7 +348,7 @@ const indexHandler = (response, callback) => {
     </table>
     <div style="height: 10px;z-index: 77777;background: grey;width: 100%;margin-top: 26px;position: fixed;"></div>
     <table id="resultTable">
-    <tbody style="top:10px !important;position:relative;">
+    <tbody style="margin-top:10px !important;position:absolute; margin-bottom:30px;">
     <tr>
     ${(function(){
     let tempHTML = ``;
